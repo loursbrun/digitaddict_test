cd892a71b53918ab231441c53620e4d3af4af515
Fabien Brun <fabienbrun@MacBook-Pro-de-Fabien.local>
2021-04-10 23:22:49 +0200

    fix menu top

--- eddy/src/components/DesktopMenu.js
+++ eddy/src/components/DesktopMenu.js
@@ -13,59 +13,55 @@ import {
     Menu,
     Segment,
     Label
-  } from 'semantic-ui-react'
+} from 'semantic-ui-react'
 
 export default props => {
-  return (
-    <Container textAlign='center' style={{marginTop: "1em"}}>
-            <Grid  stackable  >
-              <Grid  >
-                <List link >
-                <Image src={ logoImage }/>
-                </List>
-              </Grid>
-             
-              <Grid width={2} className="list-column desktop-list-link">
-                <List link >
-                  <List.Item as='a'><p className="list-item desktop-link">étude en ligne</p></List.Item>
-                </List>
-              </Grid>
-              <Grid  className="list-column desktop-list-link">
-               l
-              </Grid>
-              <Grid width={2} className="list-column desktop-list-link">
-                <List link >
-                  <List.Item as='a'><p className="list-item desktop-link">guides</p></List.Item>
-                </List>
-              </Grid>
-              <Grid  className="list-column desktop-list-link">
-               l
-              </Grid>
-              <Grid width={2} className="list-column desktop-list-link" >
-                <List link >
-                  <List.Item as='a' ><p className="list-item desktop-link">actualités</p></List.Item>
-                </List>
-              </Grid>
-              <Grid  className="list-column desktop-list-link">
-               l
-              </Grid>
-              <Grid width={3} className="list-column desktop-list-link"  >
-                <List link   >
-                  <List.Item as='a' ><p className="list-item desktop-link">qui sommes-nous ?</p></List.Item>
-                </List>
-              </Grid>
-              <Grid  className="list-column desktop-list-link">
-               l
-              </Grid>
-              <Grid width={2} className="list-column desktop-list-link">
-                <List link >
-                  <List.Item as='a'><p className="list-item desktop-link">contact</p></List.Item>
-                </List>
-              </Grid>
+    return (
+        <Container textAlign='center' style={{ marginTop: "1em" }} >
+
+
+
+            <Menu borderless className="menu-desktop">
+                <Menu.Item >
+                     <Image src={ logoImage } size='small'/>
+                 </Menu.Item>
               
-            </Grid>
-          </Container>
-  );
+                 <Menu.Menu position='right'>
+                 <Menu.Item>
+                    <List link >
+                    <List.Item as='a'><p className="list-item desktop-link">étude en ligne</p></List.Item>
+                    </List>
+                 </Menu.Item>
+                 <div style={{margin:"auto"}}>I</div>
+                 <Menu.Item>
+                    <List link >
+                    <List.Item as='a'><p className="list-item desktop-link">guides</p></List.Item>
+                    </List>
+                 </Menu.Item>
+                 <div style={{margin:"auto"}}>I</div>
+                 <Menu.Item>
+                    <List link >
+                    <List.Item as='a'><p className="list-item desktop-link">actualités</p></List.Item>
+                    </List>
+                 </Menu.Item>
+                 <div style={{margin:"auto"}}>I</div>
+                 <Menu.Item>
+                    <List link >
+                    <List.Item as='a'><p className="list-item desktop-link">qui sommes-nous ?</p></List.Item>
+                    </List>
+                 </Menu.Item>
+                 <div style={{margin:"auto"}}>I</div>
+                 <Menu.Item>
+                    <List link >
+                    <List.Item as='a'><p className="list-item desktop-link">contact</p></List.Item>
+                    </List>
+                 </Menu.Item>
+                
+                </Menu.Menu>
+            </Menu>
+
+        </Container>
+    );
 };
 
 
